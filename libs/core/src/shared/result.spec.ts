f6aed3322cd7858e2eef035c6bc03d8c5fabf7e6
Benjamin Rae <benjaminrae93@gmail.com>
2024-01-24 20:55:13 +0100

    feat(core): create Result

---
+++ libs/core/src/shared/result.spec.ts
@@ -0,0 +1,35 @@
+import { Result } from './result';
+
+describe('Result', () => {
+  it("should throw an error if it doesn't receive a value or an error", () => {
+    expect(() => new Result({ value: undefined, error: undefined })).toThrow(
+      'InvalidOperation: A result must have a value or an error',
+    );
+  });
+
+  it('should throw an error if it receives a value and an error', () => {
+    expect(() => new Result({ value: 'test', error: 'test' })).toThrow(
+      'InvalidOperation: A result cannot have a value and an error',
+    );
+  });
+
+  describe('ok', () => {
+    it('should return a successful result with the received value', () => {
+      const result = Result.ok('test');
+
+      expect(result.isSuccess).toEqual(true);
+      expect(result.isFailure).toEqual(false);
+      expect(result.value).toEqual('test');
+    });
+  });
+
+  describe('failure', () => {
+    it('should return a failed result with the received error', () => {
+      const result = Result.failure('test');
+
+      expect(result.isSuccess).toEqual(false);
+      expect(result.isFailure).toEqual(true);
+      expect(result.error).toEqual('test');
+    });
+  });
+});
