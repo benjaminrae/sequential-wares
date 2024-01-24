831bcbedc87be37ce97b07a32c04b609b93c7333
Benjamin Rae <benjaminrae93@gmail.com>
2024-01-24 22:13:49 +0100

    feat(core): create Repository and ReadRepository

--- libs/core/src/shared/index.ts
+++ libs/core/src/shared/index.ts
@@ -2,5 +2,5 @@ export * from './command';
 export * from './domain-error';
 export * from './entity';
 export * from './identifier';
+export * from './repository';
 export * from './result';
-export * from './unique-identifier';
