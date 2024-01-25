2442e5f748ec916a7538d670bd80f5d37f0ebc28
Benjamin Rae <benjaminrae93@gmail.com>
2024-01-25 02:06:56 +0100

    feat(infrastructure): create PersistenceModule with Mongoose

--- libs/infrastructure/src/config/configuration.ts
+++ libs/infrastructure/src/config/configuration.ts
@@ -2,4 +2,8 @@ import { Configuration } from './types';
 
 export const configuration = (): Configuration => ({
   port: Number(process.env.PORT) || 3000,
+  database: {
+    uri: process.env.MONGO_URI || 'mongodb://localhost:27017/sequential-wares',
+    debug: process.env.MONGO_DEBUG === 'true',
+  },
 });
---
+++ libs/infrastructure/src/persistence/persistence.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Module({
+  providers: [],
+  exports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+      }),
+    }),
+  ],
+})
+export class PersistenceModule {}
