e0f6b5e2282312f2f0612723d75af92a1745f0ef
Benjamin Rae <benjaminrae93@gmail.com>
2024-01-25 01:13:22 +0100

    feat(infrastructure): configure Open Api docs

--- libs/infrastructure/src/api/http/health/health.controller.ts
+++ libs/infrastructure/src/api/http/health/health.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
 
+@ApiTags('health')
 @Controller('health')
 export class HealthController {
   constructor(private health: HealthCheckService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { InfrastructureModule } from '@app/infrastructure';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(InfrastructureModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Sequential Wares API')
+    .setDescription('Clean Ecommerce POC')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
 
   const configService = app.get(ConfigService);
   const port = configService.get('port');
